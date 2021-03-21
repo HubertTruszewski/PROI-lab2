a1f955d3cf508010b54efc95bb7c22d9a7ed4279
Hubert Truszewski <01150052@pw.edu.pl>
2021-03-21 15:26:42 +0100

    Add implentation of FillArray function

--- FillArrayLib/FillArrayLib.cpp
+++ FillArrayLib/FillArrayLib.cpp
@@ -27,5 +27,49 @@ void fillArrayUp(std::array<std::array<int, maxColumns>, maxRows>& array, size_t
 
 void fillArray(std::array<std::array<int, maxColumns>, maxRows>& array, size_t rows, size_t columns)
 {
+	int diagonals = diagonals_number(columns, rows);
+	int value = 1;
+	int start_down_row = rows - 1;
+	int start_down_column = columns - 1;
+	int start_up_row = rows - 1;
+	int start_up_column = columns - 2;
+	for (int i = 0; i < diagonals; i++)
+	{
+		if (i % 2 == 0) // dla parzystych
+		{
+			fillArrayDown(array, rows, columns, start_down_row, start_down_column, value);
 
+			if (start_down_row > 1)
+			{
+				start_down_row -= 2;
+			}
+			else if (start_down_row == 1)
+			{
+				start_down_row--;
+				start_down_column--;
+			}
+			else
+			{
+				start_down_column -= 2;
+			}
+		}
+		else
+		{
+			fillArrayUp(array, rows, columns, start_up_row, start_up_column, value);
+
+			if (start_up_column > 1)
+			{
+				start_up_column -= 2;
+			}
+			else if (start_up_column == 1)
+			{
+				start_up_column--;
+				start_up_row--;
+			}
+			else
+			{
+				start_up_row -= 2;
+			}
+		}
+	}
 }
